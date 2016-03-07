9ccf5b813fb3d31bfe39428a9fe7cc379a04245d
zongyun <abc708095258@gmail.com>
2016-03-07 20:18:45 +0800

    HW02
    HW02

---
+++ BigDataCGUIM/0307.R
@@ -0,0 +1,35 @@
+1+1
+2^3
+5%%3
+a<-4
+b<-2
+a+b
+x+x
+y<-c(1,2,3,4)
+x+y
+z<-c(1,2,3,4,5,6)
+x+z
+
+cos(30)
+round(64.7)
+class(b)
+is.character(a)
+string<-"How are you"
+length(string)
+nchar(string)
+1.4:5.6
+seq(1,9)
+rep(1:5, time=3)
+x<-1:30
+sum(x)
+mean(x)
+max(x)
+min(x)
+median(x)
+round(sd(x),digits = 3)
+summary(x)
+sort(x)
+sort(x,decreasing = T)
+matrix(nrow = 2,ncol = 3)
+matrix("a",nrow = 2,ncol = 3)
+dim(matrix(1:6,nrow = 2,ncol = 3))
